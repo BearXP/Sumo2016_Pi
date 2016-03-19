1d9f56725522320c2e97d1a6c452062f41a13bcb
BearXP <president.evans@gmail.com>
2016-03-19 16:09:47 +1100

    Corrected formatting (linter)

--- Main/Game.py
+++ Main/Game.py
@@ -1,8 +1,9 @@
 
 # Import a library of functions called 'pygame'
+import pygame
 import Graphics
 import Input
- 
+
 # Initialize the game engine
 Graphics.init(480, 320)
 Input.init()
@@ -10,24 +11,23 @@ Input.init()
 # Loop until the user clicks the close button.
 done = False
 loopCount = 0
- 
+
 # Loop as long as done == False
 while not done:
     loopCount += 1
     Input.update()
-    if( Input.Trigger( Input.Close ) ):
+    if(Input.Trigger(Input.Close)):
         done = True
-            
     # All drawing code happens after the for loop and but
     # inside the main while not done loop.
- 
+
     # Clear the screen and set the screen background
-    if( ( int(loopCount / 15) % 2 ) == 1):
+    if((int(loopCount / 15) % 2) == 1):
         Graphics.screen.fill(Graphics.GREEN)
     else:
         Graphics.screen.fill(Graphics.RED)
     Graphics.update()
 
+
 # Close window and exit
 pygame.quit()
-    
--- Main/Input.py
+++ Main/Input.py
@@ -1,13 +1,13 @@
-#=========================================================
+# =========================================================
 # ** INPUT
-#--------------------------------------------------------
+# --------------------------------------------------------
 # HISTORY:
 #    Created By:   Mark Evans
 #    Date Created: 19/Mar/2016
 # DESCRIPTION:
 #    Handles external interfaces, expecting joystick,
 #     keyboard, and the screen (close button).
-#    Abstracts them into a simple 4 direction input & 
+#    Abstracts them into a simple 4 direction input &
 #     4 buttons
 #      - Up                    - Down
 #      - Left                  - Right
@@ -19,16 +19,16 @@
 #    while(1):
 #        Input.update()
 #        If( Input.Trigger( Input.Up) ):
-#=========================================================
+# =========================================================
 
-#---------------------------------------------------
+# ---------------------------------------------------
 # * Libraries
-#---------------------------------------------------
+# ---------------------------------------------------
 import pygame
 
-#---------------------------------------------------
+# ---------------------------------------------------
 # * Public Constants
-#---------------------------------------------------
+# ---------------------------------------------------
 Up = 1
 Down = 2
 Left = 3
@@ -39,24 +39,32 @@ C = 7
 Start = 8
 Close = 10
 
-#---------------------------------------------------
+# ---------------------------------------------------
 # * Private variables
-#---------------------------------------------------
-_joystickThreshold = 0.5                    # abs(joystick) direction must be above x before engaging
-_holdCount = [0]*100                     # How long a button has been held down
-_buttonPressed = [0]* 100                # Whether a button has been pressed
-pygame.joystick.init()                      # Initialize the joystick library
-_joystickCount = pygame.joystick.get_count()# Count the number of joysticks attached
+# ---------------------------------------------------
+# joystick threshold before engaging
+_joystickThreshold = 0.5
+# How long a button has been held down
+_holdCount = [0] * 100
+# Whether a button has been pressed
+_buttonPressed = [0] * 100
+pygame.joystick.init()                        # Initialize the joystick library
+# Count the number of joysticks attached
+_joystickCount = pygame.joystick.get_count()
 
-#---------------------------------------------------
+# ---------------------------------------------------
 # * Object Initialization
-#---------------------------------------------------
-def init():
-    pygame.mouse.set_visible( False )
+# ---------------------------------------------------
 
-#---------------------------------------------------
+
+def init():
+    pygame.mouse.set_visible(False)
+
+# ---------------------------------------------------
 # * Frame Update
-#---------------------------------------------------  
+# ---------------------------------------------------
+
+
 def update():
     # Clear whether the button was pressed
     for i in range(100):
@@ -79,11 +87,11 @@ def update():
                 _buttonPressed[Right] += 1
             if event.key == pygame.K_z:
                 _buttonPressed[A] += 1
-            if event.key == pygame.K_x :
+            if event.key == pygame.K_x:
                 _buttonPressed[B] = 1
-            if event.key == pygame.K_c :
+            if event.key == pygame.K_c:
                 _buttonPressed[C] = 1
-            if ( event.key == K_RETURN ):
+            if (event.key == pygame.K_RETURN):
                 _buttonPressed[Start] = 1
     # Joystick input
     pygame.joystick.init()
@@ -91,29 +99,31 @@ def update():
         joystick = pygame.joystick.Joystick(i)
         joystick.init()
         axes = joystick.get_numaxes()
-        if( axes >= 1 ):
-            if( joystick.get_axis(0) >= _joystickThreshold ):
+        if(axes >= 1):
+            if(joystick.get_axis(0) >= _joystickThreshold):
                 _buttonPressed[Up] = 1
-            if( -joystick.get_axis(0) >= _joystickThreshold ):
+            if(-joystick.get_axis(0) >= _joystickThreshold):
                 _buttonPressed[Down] = 1
-            if( -joystick.get_axis(1) >= _joystickThreshold ):
+            if(-joystick.get_axis(1) >= _joystickThreshold):
                 _buttonPressed[Left] = 1
-            if( joystick.get_axis(1) >= _joystickThreshold ):
+            if(joystick.get_axis(1) >= _joystickThreshold):
                 _buttonPressed[Right] = 1
     # Increment _holdCount if the button has been pressed
     for i in range(100):
         _holdCount[i] = _holdCount[i] * _buttonPressed[i] + _buttonPressed[i]
 
-#---------------------------------------------------
+# ---------------------------------------------------
 # * Button Triggered
-#---------------------------------------------------
+# ---------------------------------------------------
+
+
 def Trigger(direction):
     return _holdCount[direction] == 1
 
-#---------------------------------------------------
+# ---------------------------------------------------
 # * Button Held
-#---------------------------------------------------
+# ---------------------------------------------------
+
+
 def Hold(direction):
     return _holdCount[direction] > 0
-
-    
